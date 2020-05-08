b330b523c578b29db490479e13306ad80b8df22a
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-05-09 00:25:02 +0300

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,31 @@ class cat {
         this.name = name;
         this.sex = sex;
     }
+    speak (){
+        return `${this.name} says meow!`;
+    }
 }
 class dog {
     constructor(name,sex){
         this.name = name;
         this.sex = sex;
     }
+    speak(){
+        return `${this.name} says woof!`;
+    }
 }
 class bird {
     constructor(name,sex){
         this.name = name;
         this.sex = sex;
+    }
+    speak(){
+        if (this.sex == 'male'){
+            return `It\'s me! ${this.name}, the parrot!`
+        }
+        else{
+            return `${this.name} says squawk!`
+        }
+
     }
 }
